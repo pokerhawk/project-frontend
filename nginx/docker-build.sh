afea44898b229cc0c2b0c7a740283952db5b5db7
pokerhawk <eliabedosreis@gmail.com>
2025-01-15 16:41:33 -0300

    ..

--- nginx/docker-build.sh
+++ nginx/docker-build.sh
@@ -1,4 +1,4 @@
 docker build -t nginx-server .
 
 #                    nome container  nome volume									     nome network        imagem
-docker run -d --name nginx-server -v americanfit-volume:/var/www/html -p 80:80 --network americanfit-network nginx-server
+docker run -d --name nginx-server -v project-volume:/var/www/html -p 80:80 --network project-network nginx-server
