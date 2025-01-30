08e4dd0a9b330d1730b8db68f94bd1b484f12b01
pokerhawk <eliabedosreis@gmail.com>
2025-01-30 14:34:01 -0300

    correct env import

--- src/socket.tsx
+++ src/socket.tsx
@@ -3,8 +3,8 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const userId = (window.location.href).split('/')[4];
 const userType = (window.location.href).split('/')[3];
-const viteBase = process.env.VITE_BASE_URL;
-const URL = process.env.NODE_ENV === 'production' ? undefined : `${viteBase}?type=${userType}&userId=${userId}`;
+const viteBase = import.meta.env.VITE_BASE_URL;
+const URL = import.meta.env.NODE_ENV === 'production' ? `${viteBase}?type=${userType}&userId=${userId}` : undefined;
 
 const socket = io(URL, {
   transports: ['websocket'],
