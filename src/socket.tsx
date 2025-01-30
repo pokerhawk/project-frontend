35a220e405f693066542d3c569e5d755df0a0e8b
pokerhawk <eliabedosreis@gmail.com>
2025-01-30 14:44:04 -0300

    fix init socket

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,14 +1,17 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
 const userId = (window.location.href).split('/')[4];
 const userType = (window.location.href).split('/')[3];
 const viteBase = import.meta.env.VITE_BASE_URL;
-const URL = import.meta.env.NODE_ENV === 'production' ? `${viteBase}?type=${userType}&userId=${userId}` : undefined;
+
+// "undefined" means the URL will be computed from the `window.location` object
+// in other words, it sets to localhost:5173
+const URL = import.meta.env.NODE_ENV === 'production' ? undefined : `${viteBase}?type=${userType}&userId=${userId}`;
 
 const socket = io(URL, {
   transports: ['websocket'],
-  autoConnect: false
+  autoConnect: false,
+  timeout: 10000, //10 sec
 });
 
 export default socket;
