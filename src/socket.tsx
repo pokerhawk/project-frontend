03bb12acd9613dfae307cb5e0b9fa2d1feb5dcb3
pokerhawk <eliabedosreis@gmail.com>
2025-01-30 14:47:14 -0300

    node env

--- src/socket.tsx
+++ src/socket.tsx
@@ -6,7 +6,7 @@ const viteBase = import.meta.env.VITE_BASE_URL;
 
 // "undefined" means the URL will be computed from the `window.location` object
 // in other words, it sets to localhost:5173
-const URL = import.meta.env.NODE_ENV === 'production' ? undefined : `${viteBase}?type=${userType}&userId=${userId}`;
+const URL = import.meta.env.NODE_ENV === 'production'?undefined:`${viteBase}?type=${userType}&userId=${userId}`;
 
 const socket = io(URL, {
   transports: ['websocket'],
